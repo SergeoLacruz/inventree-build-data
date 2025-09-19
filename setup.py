872ec1089533b36372c2f2835eec94b6de46c9c7
Matthias Mair <code@mjmair.com>
2025-09-20 00:38:18 +0200

    Add classifiers to setup.py for package metadata
    This makes discoverability on PyPI better

--- setup.py
+++ setup.py
@@ -30,4 +30,9 @@ setuptools.setup(
         ]
     },
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Framework :: InvenTree",
+    ],
 )
